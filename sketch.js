3f3d0356339e892750b171a315d3497bdc404f7b
mehulshooter2 <74129739+mehulshooter2@users.noreply.github.com>
2020-11-09 22:52:42 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var bullet,wall;
+var thickness;
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+  bullet=createSprite(50,200,50,50);
+
+wall=createSprite(1200,200,thickness,height/2);
+wall.shapeColor=color(80,80,80);
+
+thickness=random(22,83);
+speed=random(223,321);
+weight=random(30,52);
+
+}
+
+function draw() {
+  background(0,0,0);  
+
+bullet.velocityX=speed;
+
+
+if(hasCollided(bullet,wall))
+{
+bullet.velocityX=0;
+var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+
+if(damage>10)
+{
+wall.shapeColor=color(255,0,0);
+}
+
+if(damage<10)
+{
+wall.shapeColor=color(0,255,0);
+}
+}
+  drawSprites();
+}
+
+
+function hasCollided(bullet,wall){
+bulletRightEdge=bullet.x+bullet.width;
+wallLeftEdge=wall.x;
+if(bulletRightEdge>=wallLeftEdge){
+return true;
+}
+return false;
+}
